3fc63aca29bf23d63d3dce943dee55e3241a382c
LuisDev01Lab <luisqrja@gmail.com>
2024-12-11 22:30:51 -0500

    feat: Init project dragon ball z characters

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+
+  {
+    rules: {
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+    },
+  },
+
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/block-order': [
+        'error',
+        {
+          order: ['script[setup]', 'template', 'style'],
+        },
+      ],
+      'vue/script-indent': [
+        'error',
+        2,
+        {
+          baseIndent: 1,
+          switchCase: 1,
+        },
+      ],
+    },
+  },
+]
